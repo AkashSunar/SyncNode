dde7eec3b1a69fafcd457c343fc4f4ca6edb91d0
AkashSunar <sunarakash1122@gmail.com>
2026-07-28 23:57:16 +0545

    mesage module included in app module

--- chat-backend/src/app.module.ts
+++ chat-backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { RoomModule } from './room/room.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, RoomModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, RoomModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
